25aa2250135307abb5f79be6ac6113b701a3af9e
Mike3361 <quaj2_15@uni.worc.ac.uk>
2018-02-09 15:00:03 +0000

    Backend

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,5 @@
 <!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
+Backendless.initApp("7B93C889-5309-F425-FF2A-B39F7B5AE100","F89B1CF6-AB1D-AAD8-FF73-DFCD2A226100");
 
 $(document).on("pagecreate","#pageone",function(){
   
